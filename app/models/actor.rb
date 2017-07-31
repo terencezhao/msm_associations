62b53e4de33b7330d808a39d5d57e3ceb3b2ed7d
Terence Zhao <terencezhao@chicagobooth.edu>
2017-07-31 04:38:18 +0000

    ci isn't working

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -11,6 +11,9 @@
 #  updated_at :datetime         not null
 #
 
+    # ruby has_many :movies, :class_name => "Movie", :foreign_key => "director_id"
+
+
 class Actor < ApplicationRecord
 #  - name: must be present; must be unique in combination with dob
     validates :name, :presence => true, :uniqueness => { :scope => :dob }
